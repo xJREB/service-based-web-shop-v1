7f207f65956f21c28ca94dc13a18b24ac07cf4ee
Justus Bogner <justus.bogner@dxc.com>
2019-03-18 17:15:47 +0100

    Initial commit for version 1

---
+++ scripts/_sonar-analyze-all.sh
@@ -0,0 +1,12 @@
+cd ../src/CategorySrv/
+mvn sonar:sonar
+cd ../src/CustomerSrv/
+mvn sonar:sonar
+cd ../src/NotificationSrv/
+mvn sonar:sonar
+cd ../src/OrderSrv/
+mvn sonar:sonar
+cd ../src/ProductSrv/
+mvn sonar:sonar
+cd ../src/WarehouseSrv/
+mvn sonar:sonar
---
+++ scripts/_sonar-build-and-analyze-all.sh
@@ -0,0 +1,12 @@
+cd ../src/CategorySrv/
+mvn clean verify sonar:sonar
+cd ../src/CustomerSrv/
+mvn clean verify sonar:sonar
+cd ../src/NotificationSrv/
+mvn clean verify sonar:sonar
+cd ../src/OrderSrv/
+mvn clean verify sonar:sonar
+cd ../src/ProductSrv/
+mvn clean verify sonar:sonar
+cd ../src/WarehouseSrv/
+mvn clean verify sonar:sonar
